854f7f3c75a96df1f9c8b9ef5d155f4430858a05
Hannah Redler <hannahjredler@gmail.com>
2024-08-08 18:56:18 +0200

    remove consoles and events

--- Part1/contracts/MerkleTree.sol
+++ Part1/contracts/MerkleTree.sol
@@ -3,18 +3,12 @@ pragma solidity ^0.8.0;
 
 import { PoseidonT3 } from "./Poseidon.sol"; //an existing library to perform Poseidon hash on solidity
 import "./verifier.sol"; //inherits with the MerkleTreeInclusionProof verifier contract
-import "hardhat/console.sol";
 
 contract MerkleTree is Groth16Verifier {
     uint256[] public hashes; // the Merkle tree in flattened array form
     uint256 public index = 0; // the current index of the first unfilled leaf
     uint256 public root; // the current Merkle root
-
-    event PrintRandom();
-    event WhatsThis(uint256[] hashes);
     constructor() {
-
-        emit PrintRandom();
         
         for (uint i=0; i< 8; i++) {
             hashes.push(0);
@@ -22,7 +16,6 @@ contract MerkleTree is Groth16Verifier {
 
         uint currPointer = 0;
         uint currArraySize = 8;
-
 
         while (currArraySize > 1) {
             for (uint i = 0; i < currArraySize/2; i++) {
@@ -32,11 +25,6 @@ contract MerkleTree is Groth16Verifier {
             currArraySize = currArraySize/2;
         }
 
-        emit WhatsThis(hashes);
-
-        console.log("Hereeeeee");
-        console.log(hashes.length, hashes[hashes.length-1]);
-
         root = hashes[hashes.length - 1];
     }
 
@@ -45,8 +33,6 @@ contract MerkleTree is Groth16Verifier {
         if (index == 4) revert("Tree full!");
         hashes[index] = hashedLeaf;
         
-        console.log("hashed leaf", hashedLeaf, index, hashes[index]);
-
         uint currArraySize = 8;
         uint currArrayStartPointer = 0;
         uint currIndex = index;
@@ -54,7 +40,6 @@ contract MerkleTree is Groth16Verifier {
         while (currArraySize > 1) {
             uint toAdd = (currIndex-currArrayStartPointer)/2 ; // this is how far from the start of the new array I need to go
             uint newCurrIndex = currArrayStartPointer + currArraySize + toAdd;
-            console.log("stats", currArraySize, toAdd, newCurrIndex);
             if (index % 2 == 0) {
                 // left leaf
                 hashes[newCurrIndex] = PoseidonT3.poseidon([hashes[currIndex], hashes[currIndex+1]]);
